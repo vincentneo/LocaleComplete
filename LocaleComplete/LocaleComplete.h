8432823852938f637f6baf283dba8b4cc60ce7cd
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2020-01-12 21:40:34 +0800

    Initial Commit

---
+++ LocaleComplete/LocaleComplete.h
@@ -0,0 +1,19 @@
+//
+//  LocaleComplete.h
+//  LocaleComplete
+//
+//  Created by Vincent on 12/1/20.
+//  Copyright © 2020 Vincent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocaleComplete.
+FOUNDATION_EXPORT double LocaleCompleteVersionNumber;
+
+//! Project version string for LocaleComplete.
+FOUNDATION_EXPORT const unsigned char LocaleCompleteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocaleComplete/PublicHeader.h>
+
+
