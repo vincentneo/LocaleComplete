c319eebc044ff6505190f31d81f0e5d1ce9f719e
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2020-05-29 00:33:53 +0800

    Add summary for initializers

--- LocaleComplete/LocaleComplete.swift
+++ LocaleComplete/LocaleComplete.swift
@@ -9,20 +9,24 @@
 import Foundation
 
 public extension Locale {
+    /// Creates a locale with the strongly typed specified identifier.
     init(id: LocaleID) {
         self.init(identifier: id.rawValue)
     }
     
+    /// Creates a new instance by the name of the locale.
     init(key: LocaleKeys) {
         self.init(identifier: key.rawValue)
     }
 }
 
 public extension NSLocale {
+    /// Initializes a locale using a given strongly typed identifier.
     convenience init(id: LocaleID) {
         self.init(localeIdentifier: id.rawValue)
     }
     
+    /// Initializes a locale using a the name of the locale.
     convenience init(key: LocaleKeys) {
         self.init(localeIdentifier: key.rawValue)
     }
